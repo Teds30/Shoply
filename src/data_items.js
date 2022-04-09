865a9d85f56ba94da84c8b3c83b646ff46fe6a68
Ted30 <teddymarcenaje@gmail.com>
2022-04-10 01:15:21 +0800

    updated data structure

--- src/data_items.js
+++ src/data_items.js
@@ -3,8 +3,56 @@ export const DUMMY_ITEMS = [
         id: '1',
         name: 'ORIGINAL BRAND TSHIRT',
         price: 299,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png',
-        star: 4,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'black'
+            },
+            
+            {
+                color_id: 2,
+                color: 'gray'
+            }
+        ],
+        total_star: 4,
         reviews: [
             {
                 id: 1,
@@ -32,8 +80,51 @@ export const DUMMY_ITEMS = [
         id: '2',
         name: 'LIMITED BRAND TSHIRT',
         price: 299,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://i.pinimg.com/originals/bd/ef/cb/bdefcbc72735f64db17f3250b1e64245.png',
-        star: 5,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'orange'
+            }
+        ],
+        total_star: 5,
         reviews: [
             {
                 id: 1,
@@ -51,8 +142,51 @@ export const DUMMY_ITEMS = [
         id: '3',
         name: 'VALORANT PRINTED TSHIRT',
         price: 299,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/white-tshirt-n0j.png',
-        star: 3,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: '#fff'
+            },
+        ],
+        total_star: 3,
         reviews: [
             {
                 id: 1,
@@ -70,8 +204,51 @@ export const DUMMY_ITEMS = [
         id: '4',
         name: 'LENI TSHIRT',
         price: 299,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/purepng.com-t-shirtclothingt-shirtfashion-dress-shirt-cloth-tshirt-631522326894filwv.png',
-        star: 5,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'pink'
+            }
+        ],
+        total_star: 5,
         reviews: [
             {
                 id: 1,
@@ -89,8 +266,51 @@ export const DUMMY_ITEMS = [
         id: '5',
         name: 'NAMARCOS PRINTED TSHIRT',
         price: 499,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/purepng.com-red-t-shirtclothingred-t-shirtfashion-dress-shirt-cloth-tshirt-631522326799mcfdo.png',
-        star: 1,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'red'
+            }
+        ],
+        total_star: 1,
         reviews: [
             {
                 id: 1,
@@ -108,8 +328,51 @@ export const DUMMY_ITEMS = [
         id: '6',
         name: 'SPECIAL THEME TSHIRT',
         price: 499,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/purepng.com-men-t-shirtclothingmen-t-shirtfashion-dress-shirt-cloth-tshirt-631522326839zoswy.png',
-        star: 5,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'purple'
+            }
+        ],
+        total_star: 5,
         reviews: [
             {
                 id: 1,
@@ -127,8 +390,51 @@ export const DUMMY_ITEMS = [
         id: '7',
         name: 'COOL TSHIRT',
         price: 799,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/purepng.com-grey-polo-shirtpolo-shirtcottongarmentsfebricgrey-142152639264876l00.png',
-        star: 4,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'grey'
+            }
+        ],
+        total_star: 4,
         reviews: [
             {
                 id: 1,
@@ -146,8 +452,51 @@ export const DUMMY_ITEMS = [
         id: '8',
         name: 'AMAZING TSHIRT',
         price: 599,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://purepng.com/public/uploads/large/plain-black-polo-shirt-pab.png',
-        star: 4,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'black'
+            }
+        ],
+        total_star: 4,
         reviews: [
             {
                 id: 1,
@@ -165,8 +514,51 @@ export const DUMMY_ITEMS = [
         id: '9',
         name: 'TESTING NEW ITEM',
         price: 99,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: '',
-        star: 4,
+        previews: [
+            // {
+            //     preview_id: 1,
+            //     preview_image: '',
+            // }
+        ],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
+            },
+            {
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 1,
+                size: 'S'
+            },
+            {
+                size_id: 2,
+                size: 'M'
+            },
+            {
+                size_id: 3,
+                size: 'L'
+            },
+            {
+                size_id: 4,
+                size: 'XL'
+            }
+        ],
+        colors: [
+            {
+                color_id: 1,
+                color: 'red'
+            }
+        ],
+        total_star: 4,
         reviews: [
             {
                 id: 1,
