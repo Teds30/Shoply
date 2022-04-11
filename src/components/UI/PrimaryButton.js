d776365b9a1d95ad6c64f1c9e5ba92a7b7fd097c
Ted30 <teddymarcenaje@gmail.com>
2022-04-12 00:05:50 +0800

    new components

---
+++ src/components/UI/PrimaryButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './PrimaryButton.module.css'
+
+const PrimaryButton = (props) => {
+  return (
+    <button className={classes.primarybutton} style={{width: props.width}} onClick={props.onClick} type={props.submit}>{props.children}</button>
+  )
+}
+
+export default PrimaryButton
