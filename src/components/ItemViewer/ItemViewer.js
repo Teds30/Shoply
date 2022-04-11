b1578b94d867d25825b6540aec7f50cc93fb19ae
Ted30 <teddymarcenaje@gmail.com>
2022-04-12 00:03:44 +0800

    added new Alert(ft. MUI Backdrop)

--- src/components/ItemViewer/ItemViewer.js
+++ src/components/ItemViewer/ItemViewer.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { Fragment, useState, useContext } from 'react'
 import { useParams } from "react-router-dom";
+
+import Backdrop from '@mui/material/Backdrop';
+import Alert from '../UI/Alert';
+import { IconContext } from 'react-icons'
+import {  BsCheckLg } from 'react-icons/bs'
 
 import ItemViewerContent from './ItemViewerContent'
 
 import classes from './ItemViewer.module.css'
+
+// import CartContext from '../../context/cart-context'
+
+// import Alert from '../UI/Alert'
+// import useDidMountEffect from '../../custom_hook/useDidMountEffect'
+// import { IconContext } from 'react-icons'
+// import {  BsCheckLg } from 'react-icons/bs'
 
 import { DUMMY_ITEMS } from '../../data_items';
 
 const ItemViewer = (props) => {
 
     const { id } = useParams()
+
+    const [showAlert, setShowAlert] = useState(false)
+
+    const showAlertHandler = () => {
+        setShowAlert(true)
+    }
+    const hideAlertHandler = () => {
+        setShowAlert(false)
+    }
 
 
     const item = DUMMY_ITEMS.find(
@@ -20,15 +41,28 @@ const ItemViewer = (props) => {
 
 
     return (
-        <div className={classes['main-container']}>
+        <Fragment>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={showAlert}
+                onClick={hideAlertHandler}
+            >
+                <Alert label='Added to Cart' icon={
+                    <IconContext.Provider value={{ color: "#7DCB98", size: '18px' }}>
+                        <div>
+                            <BsCheckLg />
+                        </div>
+                    </IconContext.Provider>}>
+                </Alert>
+            </Backdrop>
+            <div className={classes['main-container']}>
 
-            
-
-            {!hasItem && (<h1>404</h1>)}
-            {hasItem &&
-                <ItemViewerContent item={item}/>
-            }
-        </div>
+                {!hasItem && (<h1>404</h1>)}
+                {hasItem &&
+                    <ItemViewerContent item={item} onShowAlert={showAlertHandler} onHideAlert={hideAlertHandler} />
+                }
+            </div>
+        </Fragment>
 
     )
 }
