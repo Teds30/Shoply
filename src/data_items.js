31528f6261fdfc78b2c5ba78b892c6382993897d
Ted30 <teddymarcenaje@gmail.com>
2022-04-11 23:59:09 +0800

    updated some fields

--- src/data_items.js
+++ src/data_items.js
@@ -6,10 +6,14 @@ export const DUMMY_ITEMS = [
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
         image: 'https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png',
         previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
+            {
+                preview_id: 1,
+                preview_image: 'https://i.pinimg.com/originals/bd/ef/cb/bdefcbc72735f64db17f3250b1e64245.png',
+            },
+            {
+                preview_id: 2,
+                preview_image: 'https://purepng.com/public/uploads/large/white-tshirt-n0j.png',
+            },
         ],
         payment: [
             {
@@ -25,31 +29,38 @@ export const DUMMY_ITEMS = [
         ],
         sizes: [
             {
-                size_id: 1,
-                size: 'S'
+                size_id: 's1',
+                size: 'S',
             },
             {
-                size_id: 2,
+                size_id: 's2',
                 size: 'M'
             },
             {
-                size_id: 3,
+                size_id: 's3',
                 size: 'L'
             },
             {
-                size_id: 4,
+                size_id: 's4',
                 size: 'XL'
             }
         ],
         colors: [
             {
-                color_id: 1,
-                color: 'black'
+                color_id: 'c1',
+                color: 'black',
+                color_itemimage: 'https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png', 
             },
             
             {
-                color_id: 2,
-                color: 'gray'
+                color_id: 'c2',
+                color: 'orange',
+                color_itemimage: 'https://i.pinimg.com/originals/bd/ef/cb/bdefcbc72735f64db17f3250b1e64245.png'
+            },
+            {
+                color_id: 'c3',
+                color: 'white',
+                color_itemimage: 'https://purepng.com/public/uploads/large/white-tshirt-n0j.png'
             }
         ],
         total_star: 4,
@@ -63,9 +74,55 @@ export const DUMMY_ITEMS = [
                     message: 'Exactly fits on me! Great product :)',
                     image: ''
                 }
+            }
+        ]
+    },
+
+
+
+
+    {
+        id: '2',
+        name: 'POLOSKIE',
+        price: 599,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
+        image: 'https://purepng.com/public/uploads/large/plain-black-polo-shirt-pab.png',
+        previews: [],
+        payment: [
+            {
+                payment_id: 1,
+                payment_icon: '',
+                payment_method: 'Cash On Delivery',
             },
             {
-                id: 2,
+                payment_id: 2,
+                payment_icon: '',
+                payment_method: 'GCash'
+            }
+        ],
+        sizes: [
+            {
+                size_id: 's1',
+                size: 'S',
+            },
+            {
+                size_id: 's2',
+                size: 'M'
+            },
+            {
+                size_id: 's3',
+                size: 'L'
+            },
+            {
+                size_id: 's4',
+                size: 'XL'
+            }
+        ],
+        colors: [],
+        total_star: 4,
+        reviews: [
+            {
+                id: 1,
                 fromUID: '123456789',
                 star: 4,
                 comment:
@@ -73,21 +130,18 @@ export const DUMMY_ITEMS = [
                     message: 'Exactly fits on me! Great product :)',
                     image: ''
                 }
-            },
+            }
         ]
     },
+
+
     {
-        id: '2',
-        name: 'LIMITED BRAND TSHIRT',
-        price: 299,
+        id: '3',
+        name: 'GREY POLO',
+        price: 599,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: 'https://i.pinimg.com/originals/bd/ef/cb/bdefcbc72735f64db17f3250b1e64245.png',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
+        image: 'https://purepng.com/public/uploads/large/purepng.com-grey-polo-shirtpolo-shirtcottongarmentsfebricgrey-142152639264876l00.png',
+        previews: [],
         payment: [
             {
                 payment_id: 1,
@@ -118,18 +172,13 @@ export const DUMMY_ITEMS = [
                 size: 'XL'
             }
         ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'orange'
-            }
-        ],
+        colors: [],
         total_star: 5,
         reviews: [
             {
                 id: 1,
                 fromUID: '123456789',
-                star: 4,
+                star: 5,
                 comment:
                 {
                     message: 'Exactly fits on me! Great product :)',
@@ -138,68 +187,7 @@ export const DUMMY_ITEMS = [
             }
         ]
     },
-    {
-        id: '3',
-        name: 'VALORANT PRINTED TSHIRT',
-        price: 299,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: 'https://purepng.com/public/uploads/large/white-tshirt-n0j.png',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
-        payment: [
-            {
-                payment_id: 1,
-                payment_icon: '',
-                payment_method: 'Cash On Delivery',
-            },
-            {
-                payment_id: 2,
-                payment_icon: '',
-                payment_method: 'GCash'
-            }
-        ],
-        sizes: [
-            {
-                size_id: 1,
-                size: 'S'
-            },
-            {
-                size_id: 2,
-                size: 'M'
-            },
-            {
-                size_id: 3,
-                size: 'L'
-            },
-            {
-                size_id: 4,
-                size: 'XL'
-            }
-        ],
-        colors: [
-            {
-                color_id: 1,
-                color: '#fff'
-            },
-        ],
-        total_star: 3,
-        reviews: [
-            {
-                id: 1,
-                fromUID: '123456789',
-                star: 4,
-                comment:
-                {
-                    message: 'Exactly fits on me! Great product :)',
-                    image: ''
-                }
-            }
-        ]
-    },
+
     {
         id: '4',
         name: 'LENI TSHIRT',
@@ -242,12 +230,7 @@ export const DUMMY_ITEMS = [
                 size: 'XL'
             }
         ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'pink'
-            }
-        ],
+        colors: [],
         total_star: 5,
         reviews: [
             {
@@ -304,261 +287,8 @@ export const DUMMY_ITEMS = [
                 size: 'XL'
             }
         ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'red'
-            }
-        ],
+        colors: [],
         total_star: 1,
-        reviews: [
-            {
-                id: 1,
-                fromUID: '123456789',
-                star: 4,
-                comment:
-                {
-                    message: 'Exactly fits on me! Great product :)',
-                    image: ''
-                }
-            }
-        ]
-    },
-    {
-        id: '6',
-        name: 'SPECIAL THEME TSHIRT',
-        price: 499,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: 'https://purepng.com/public/uploads/large/purepng.com-men-t-shirtclothingmen-t-shirtfashion-dress-shirt-cloth-tshirt-631522326839zoswy.png',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
-        payment: [
-            {
-                payment_id: 1,
-                payment_icon: '',
-                payment_method: 'Cash On Delivery',
-            },
-            {
-                payment_id: 2,
-                payment_icon: '',
-                payment_method: 'GCash'
-            }
-        ],
-        sizes: [
-            {
-                size_id: 1,
-                size: 'S'
-            },
-            {
-                size_id: 2,
-                size: 'M'
-            },
-            {
-                size_id: 3,
-                size: 'L'
-            },
-            {
-                size_id: 4,
-                size: 'XL'
-            }
-        ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'purple'
-            }
-        ],
-        total_star: 5,
-        reviews: [
-            {
-                id: 1,
-                fromUID: '123456789',
-                star: 4,
-                comment:
-                {
-                    message: 'Exactly fits on me! Great product :)',
-                    image: ''
-                }
-            }
-        ]
-    },
-    {
-        id: '7',
-        name: 'COOL TSHIRT',
-        price: 799,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: 'https://purepng.com/public/uploads/large/purepng.com-grey-polo-shirtpolo-shirtcottongarmentsfebricgrey-142152639264876l00.png',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
-        payment: [
-            {
-                payment_id: 1,
-                payment_icon: '',
-                payment_method: 'Cash On Delivery',
-            },
-            {
-                payment_id: 2,
-                payment_icon: '',
-                payment_method: 'GCash'
-            }
-        ],
-        sizes: [
-            {
-                size_id: 1,
-                size: 'S'
-            },
-            {
-                size_id: 2,
-                size: 'M'
-            },
-            {
-                size_id: 3,
-                size: 'L'
-            },
-            {
-                size_id: 4,
-                size: 'XL'
-            }
-        ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'grey'
-            }
-        ],
-        total_star: 4,
-        reviews: [
-            {
-                id: 1,
-                fromUID: '123456789',
-                star: 4,
-                comment:
-                {
-                    message: 'Exactly fits on me! Great product :)',
-                    image: ''
-                }
-            }
-        ]
-    },
-    {
-        id: '8',
-        name: 'AMAZING TSHIRT',
-        price: 599,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: 'https://purepng.com/public/uploads/large/plain-black-polo-shirt-pab.png',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
-        payment: [
-            {
-                payment_id: 1,
-                payment_icon: '',
-                payment_method: 'Cash On Delivery',
-            },
-            {
-                payment_id: 2,
-                payment_icon: '',
-                payment_method: 'GCash'
-            }
-        ],
-        sizes: [
-            {
-                size_id: 1,
-                size: 'S'
-            },
-            {
-                size_id: 2,
-                size: 'M'
-            },
-            {
-                size_id: 3,
-                size: 'L'
-            },
-            {
-                size_id: 4,
-                size: 'XL'
-            }
-        ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'black'
-            }
-        ],
-        total_star: 4,
-        reviews: [
-            {
-                id: 1,
-                fromUID: '123456789',
-                star: 4,
-                comment:
-                {
-                    message: 'Exactly fits on me! Great product :)',
-                    image: ''
-                }
-            }
-        ]
-    },
-    {
-        id: '9',
-        name: 'TESTING NEW ITEM',
-        price: 99,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis nostrum blanditiis odio quisquam architecto.',
-        image: '',
-        previews: [
-            // {
-            //     preview_id: 1,
-            //     preview_image: '',
-            // }
-        ],
-        payment: [
-            {
-                payment_id: 1,
-                payment_icon: '',
-                payment_method: 'Cash On Delivery',
-            },
-            {
-                payment_id: 2,
-                payment_icon: '',
-                payment_method: 'GCash'
-            }
-        ],
-        sizes: [
-            {
-                size_id: 1,
-                size: 'S'
-            },
-            {
-                size_id: 2,
-                size: 'M'
-            },
-            {
-                size_id: 3,
-                size: 'L'
-            },
-            {
-                size_id: 4,
-                size: 'XL'
-            }
-        ],
-        colors: [
-            {
-                color_id: 1,
-                color: 'red'
-            }
-        ],
-        total_star: 4,
         reviews: [
             {
                 id: 1,
