56ae8fcba7ae31162d14ef257816c41099ba2598
Ted30 <teddymarcenaje@gmail.com>
2022-04-16 23:44:12 +0800

    added commented block for future use

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { useMediaQuery } from 'react-responsive';
 import './App.css';
 
-import CartProvider from './context/CartProvider';
 import Items from './components/Items/Items';
 import Header from './components/Layout/Header';
 import Cart from './components/Cart/Cart';
 import ItemViewer from './components/ItemViewer/ItemViewer';
 
-// import BottomNavigation from '@mui/material/BottomNavigation';
-// import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import ArchiveIcon from '@mui/icons-material/Archive';
-// import Paper from '@mui/material/Paper';
-// import HomeIcon from '@mui/icons-material/Home';
-
+// import IMPORT_DATA from './data_import';
 
 function App() {
-  // const [value, setValue] = useState(0);
 
+  // useEffect(()=> {
 
-  // const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  //   const toImport = {...IMPORT_DATA}
+  //   console.log(toImport)
+
+  //   const importData = async () => {
+
+  //     await fetch('https://shoply-25605-default-rtdb.asia-southeast1.firebasedatabase.app/ShopItems.json',
+  //     {
+  //       method: 'POST',
+  //       body: JSON.stringify(toImport)
+  //     })
+  
+  //   }
+
+  //   importData()
+    
+  // }, [])
 
   return (
-    <CartProvider>
       <Router>
 
         <Routes>
@@ -37,26 +43,8 @@ function App() {
         </Routes>
 
         <Header />
-        {/* {!isMobile ? (<Header />) :
-          <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-            <BottomNavigation
-              showLabels
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-            >
-              <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-              <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
-            </BottomNavigation>
-          </Paper>
-        } */}
 
       </Router>
-    </CartProvider>
-
-
   );
 }
 
