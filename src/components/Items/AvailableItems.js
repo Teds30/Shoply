ea8553878bf302985c9befccd2cb0c2e2275a0c0
Ted30 <teddymarcenaje@gmail.com>
2023-01-21 18:06:28 +0800

    removed dummy data

--- src/components/Items/AvailableItems.js
+++ src/components/Items/AvailableItems.js
@@ -4,25 +4,15 @@ import ContentItem from './ContentItem/ContentItem'
 
 import classes from './AvailableItems.module.css'
 
-import { DUMMY_ITEMS } from '../../data_items'
-
 const AvailableItems = (props) => {
-
     const itemsList = props.items.map((item) => {
-        return (
-            <ContentItem
-                key={item.id}
-                item={item}
-            />
-        )
+        return <ContentItem key={item.id} item={item} />
     })
 
     return (
         <Fragment>
             <section className={classes.items}>
-                <ul>
-                    {itemsList}
-                </ul>
+                <ul>{itemsList}</ul>
             </section>
         </Fragment>
     )
